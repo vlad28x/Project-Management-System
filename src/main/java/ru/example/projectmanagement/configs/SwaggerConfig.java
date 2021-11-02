c1f45f88211f38cc8fdb66bcb3a7fa9b7c2231de
vlad28x <vladskibin@bk.ru>
2021-11-02 23:14:36 +0300

    TASK-1: Add swagger config

---
+++ src/main/java/ru/example/projectmanagement/configs/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package ru.example.projectmanagement.configs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(
+                new Info().title("Project Management System Api").version("1.0.0").contact(
+                new Contact().name("Vladislav Skibin").email("vladskibin@bk.ru").url("https://github.com/vlad28x/")));
+    }
+}
