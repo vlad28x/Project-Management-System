b0df97db4e0b88c6926737c1affc69b0af9b4305
vlad28x <vladskibin@bk.ru>
2021-11-02 21:18:28 +0300

    TASK-1: Modify TaskController according to a dao

--- src/main/java/ru/example/projectmanagement/controllers/TaskController.java
+++ src/main/java/ru/example/projectmanagement/controllers/TaskController.java
@@ -1,34 +1,41 @@
 package ru.example.projectmanagement.controllers;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.example.projectmanagement.dto.TaskRequestDTO;
+import ru.example.projectmanagement.dto.TaskResponseDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
     @GetMapping
-    public ResponseEntity<Object> getAllTasks() {
-        return ResponseEntity.ok().build();
+    public List<TaskResponseDTO> getAllTasks() {
+        List<TaskResponseDTO> list = new ArrayList<>();
+        list.add(new TaskResponseDTO());
+        return list;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Object> getTaskById(@PathVariable Long id) {
-        return ResponseEntity.ok().build();
+    public TaskResponseDTO getTaskById(@PathVariable Long id) {
+        return new TaskResponseDTO();
     }
 
     @PostMapping
-    public ResponseEntity<Object> createTask() {
-        return ResponseEntity.ok().build();
+    @ResponseStatus(HttpStatus.CREATED)
+    public TaskResponseDTO createTask(@RequestBody TaskRequestDTO newTask) {
+        return new TaskResponseDTO();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Object> updateTask(@PathVariable Long id) {
-        return ResponseEntity.ok().build();
+    public TaskResponseDTO updateTask(@PathVariable Long id, @RequestBody TaskRequestDTO newTask) {
+        return new TaskResponseDTO();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteTask(@PathVariable Long id) {
-        return ResponseEntity.ok().build();
+    public void deleteTask(@PathVariable Long id) {
     }
 }
