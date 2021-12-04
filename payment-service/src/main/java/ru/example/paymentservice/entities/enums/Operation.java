76b006a55cedb0d7f52b4434e86da4f0393e5283
vlad28x <vlad.skibin@mail.ru>
2021-12-04 20:38:14 +0300

    TASK-5: Move payment-service to another repository

--- payment-service/src/main/java/ru/example/paymentservice/entities/enums/Operation.java
+++
@@ -1,5 +0,0 @@
-package ru.example.paymentservice.entities.enums;
-
-public enum Operation {
-    TOP_UP, SPEND
-}
--- payment-service/src/main/java/ru/example/paymentservice/repositories/AccountHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.example.paymentservice.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.example.paymentservice.entities.AccountHistory;
-
-@Repository
-public interface AccountHistoryRepository extends JpaRepository<AccountHistory, Long> {
-}
